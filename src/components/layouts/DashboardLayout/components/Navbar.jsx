4b671429215561fa479a523a664bcfbcd7c35d99
abfatahi <abfatahi.iaf@gmail.com>
2025-02-27 14:03:28 +0100

    chore: finalized navbar resposiveness

--- src/components/layouts/DashboardLayout/components/Navbar.jsx
+++ src/components/layouts/DashboardLayout/components/Navbar.jsx
@@ -1,5 +1,3 @@
-import styled from "styled-components";
-
 import { useState } from "react";
 import { useSelector } from "react-redux";
 import { userSelector } from "@/redux/reducers/user";
@@ -13,6 +11,8 @@ import SearchIcon from "@assets/icons/search.svg?react";
 import SettingsIcon from "@assets/icons/settings-info.svg?react";
 import NotificationIcon from "@assets/icons/notification.svg?react";
 import HamburgerIcon from "@assets/icons/menu.svg?react";
+
+import { NavbarContainer } from "./styles";
 
 const Navbar = () => {
   const [searchQuery, setSearchQuery] = useState("");
@@ -72,94 +72,3 @@ const NavCTAs = [
   { title: "settings icon", icon: <SettingsIcon /> },
   { title: "notification icon", icon: <NotificationIcon /> },
 ];
-
-const NavbarContainer = styled.nav`
-  position: sticky;
-  top: 0;
-  margin-left: 250px;
-  display: flex;
-  align-items: center;
-  flex-direction: column;
-  justify-content: center;
-  height: 101px;
-  width: calc(100vw - 250px);
-  background-color: var(--color-white);
-  border-bottom: 1px solid var(--color-primary-lighter);
-  z-index: var(--z-index-highest);
-  padding: 0 40px;
-
-  .navGroup {
-    width: 100%;
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-  }
-
-  @media (max-width: 768px) {
-    height: max-content;
-    padding: 16px 24px;
-    border-bottom: none;
-
-    .hideOnSmallScreen {
-      display: none;
-    }
-  }
-
-  @media (max-width: 425px) {
-    margin-left: 0;
-    width: 100%;
-  }
-
-  .showOnMobile {
-    display: none;
-
-    @media (max-width: 425px) {
-      display: block;
-    }
-  }
-
-  .showOnTablet {
-    display: none;
-
-    @media (max-width: 768px) {
-      display: block;
-    }
-  }
-
-  .mobileSearch {
-    margin-top: 1rem;
-    width: 100%;
-  }
-
-  .navCTAGroup {
-    display: flex;
-    align-items: center;
-    gap: 30px;
-
-    .profile_image {
-      width: 60px;
-      height: 60px;
-      border-radius: 50%;
-    }
-  }
-
-  .icon_with_background {
-    width: 50px;
-    height: 50px;
-    background: var(--color-gray-lighter);
-    border-radius: 50%;
-    padding: 10px;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-
-    @media (max-width: 425px) {
-      display: none;
-    }
-  }
-
-  h1 {
-    font-size: 28px;
-    font-weight: 600;
-  }
-`;
---
+++ src/components/layouts/DashboardLayout/components/styles.jsx
@@ -0,0 +1,112 @@
+import styled from "styled-components";
+
+const NavbarContainer = styled.nav`
+  position: sticky;
+  top: 0;
+  margin-left: 250px;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  justify-content: center;
+  height: 101px;
+  width: calc(100vw - 250px);
+  background-color: var(--color-white);
+  border-bottom: 1px solid var(--color-primary-lighter);
+  z-index: var(--z-index-highest);
+  padding: 0 40px;
+
+  .navGroup {
+    width: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+  }
+
+  @media (max-width: 768px) {
+    height: max-content;
+    padding: 16px 24px;
+    border-bottom: none;
+
+    .hideOnSmallScreen {
+      display: none;
+    }
+  }
+
+  @media (max-width: 425px) {
+    margin-left: 0;
+    width: 100%;
+  }
+
+  .showOnMobile {
+    display: none;
+
+    @media (max-width: 425px) {
+      display: block;
+    }
+  }
+
+  .showOnTablet {
+    display: none;
+
+    @media (max-width: 768px) {
+      display: block;
+    }
+  }
+
+  .mobileSearch {
+    margin-top: 1rem;
+    width: 100%;
+  }
+
+  .navCTAGroup {
+    display: flex;
+    align-items: center;
+    gap: 30px;
+
+    .profile_image {
+      width: 60px;
+      height: 60px;
+      border-radius: 50%;
+
+      @media (max-width: 768px) {
+        width: 40px;
+        height: 40px;
+      }
+
+      @media (max-width: 425px) {
+        width: 35px;
+        height: 35px;
+      }
+    }
+  }
+
+  .icon_with_background {
+    width: 50px;
+    height: 50px;
+    background: var(--color-gray-lighter);
+    border-radius: 50%;
+    padding: 10px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    @media (max-width: 425px) {
+      display: none;
+    }
+  }
+
+  h1 {
+    font-size: 28px;
+    font-weight: 600;
+
+    @media (max-width: 768px) {
+      font-size: 24px;
+    }
+
+    @media (max-width: 425px) {
+      font-size: 20px;
+    }
+  }
+`;
+
+export { NavbarContainer };
